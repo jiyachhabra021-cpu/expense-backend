efcfbab5e7fa83b8d4c991bb87252ebbd80c4ad3
Jiya Chhabra <jiyachhabra@192.168.1.13>
2026-07-03 14:00:31 +0530

    fix render port

--- server.js
+++ server.js
@@ -5,20 +5,19 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// simple storage for now
 let expenses = [];
 
-// add expense
 app.post("/add", (req, res) => {
     expenses.push(req.body);
     res.json({ message: "added" });
 });
 
-// get expenses
 app.get("/get", (req, res) => {
     res.json(expenses);
 });
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
